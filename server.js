ebbf92eaa156213af38ba099ebecc9d7dedc3de9
brumePyvault <oghenebrume.akpadaka@gmail.com>
2024-10-08 20:03:58 +0100

    "CORS settings updated to allow requests from all origins instead of specific ones"

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 3000; // Change this if you need to use another port
 
 // CORS settings to explicitly allow requests from a specific origin
 const corsOptions = {
-  origin: ["http://localhost:5173", "*"], // Allow only this origin
+  origin: "*", // Allow only this origin
   methods: "GET, POST, PUT, DELETE, OPTIONS",
   allowedHeaders:
     "Origin, X-Requested-With, Content-Type, Accept, Authorization",
@@ -20,7 +20,7 @@ app.use(morgan("dev"));
 
 // Middleware to handle preflight requests explicitly
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", ["http://localhost:5173", "*"]);
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
